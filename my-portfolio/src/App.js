36d2db31689b8716aa241292285aba44acd9fc20
akpante3 <obije.victor5@gmail.com>
2020-04-26 21:07:09 +0100

    welcome page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AwesomeSlider from 'react-awesome-slider';
+import Nav from './component/Nav/Nav'
+import Indroduction from './pages/introduction/Introduction'
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import 'react-awesome-slider/dist/styles.css';
+import './App.scss';
 
 function App() {
+  const list = [
+    <Indroduction />,
+    <div>2</div>,
+    <div>3</div>,
+    <div>4</div>
+  ]
+  const templates = list.map((data, index) =>
+     <div key={index} > {data}</div>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div className={['wrapper', 'toggle-nav-buttons'].join(' ')}>
+      <Nav />
+      <AwesomeSlider animation="cubeAnimation">
+          { templates }
+      </AwesomeSlider>
     </div>
   );
 }
---
+++ my-portfolio/src/pages/introduction/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Introduction.scss';
+
+function Introduction() {
+  return (
+    <div className="introduction">
+       <div id="name-intro">Hello, I am Victor Obije</div>
+       <div id="profession">I'm a Full-Stack Software Engineer</div>
+       <button id="introduction__button">View my work</button>
+    </div>
+  );
+}
+
+export default Introduction;
