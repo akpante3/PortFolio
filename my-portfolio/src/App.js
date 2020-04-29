e3ea072949bf778ec5e34d49781955a1144f2d0b
akpante3 <obije.victor5@gmail.com>
2020-04-29 20:41:02 +0100

    menu

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
-import Nav from './component/Nav/Nav';
 import Indroduction from './pages/introduction/Introduction';
 import Menu from './pages/menu/Menu';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
@@ -32,8 +31,19 @@ class App extends React.Component {
         <div>
           { this.state.showMenu ? <Menu /> : '' }
         </div>
+        <div className="menu-button">
+        { !this.state.showMenu ? (
+            <svg viewBox="0 0 100 80" width="100" height="20">
+            <rect className="menu-button__item--1" height="10"></rect>
+            <rect className="menu-button__item--2" y="30" height="10"></rect>
+            <rect className="menu-button__item--3" y="60" height="10"></rect>
+          </svg>
+        ) : (
+          <span className="menu-button__close"><a  href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></span>
+        )
+        }
+        </div>
         <div className={['wrapper', 'toggle-nav-buttons'].join(' ')}>
-            <Nav />
             <AwesomeSlider animation="cubeAnimation" selected={1} infinite={false} mobileTouch={true}>
                 { templates }
             </AwesomeSlider>
