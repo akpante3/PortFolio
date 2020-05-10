31e730b534d5c5b5a42f3b94601c568da9823251
akpante3 <obije.victor5@gmail.com>
2020-05-10 21:26:47 +0100

    contact

--- my-portfolio/src/pages/contact/Contact.js
+++ my-portfolio/src/pages/contact/Contact.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import './Contact.scss'
 
 function Contact() {
   return (
     <div className="contact">
       <div>
-        <div>Contact</div>
-        <div><a href="https://github.com/akpante3"><img src={require('../../images/icons/github.svg')} alt="hello" /></a></div>
-        <div><a href="mailto:nowhere@mozilla.org"><img src={require('../../images/icons/linkedin.svg')} alt="hello" /></a></div>
-        <div><a href="mailto:nowhere@mozilla.org"><img src={require('../../images/icons/gitlab.svg')} alt="hello" /></a></div>
-        <div><a href="mailto:nowhere@mozilla.org"><img src={require('../../images/icons/gmail.svg')} alt="hello" /></a></div>
+        <div className="contact__header">Contact</div>
+        <div className="contact__content"><a href="https://github.com/akpante3"><img src={require('../../images/icons/github.svg')} alt="hello" /></a></div>
+        <div className="contact__content"><a href="mailto:nowhere@mozilla.org"><img src={require('../../images/icons/linkedin.svg')} alt="hello" /></a></div>
+        <div className="contact__content"><a href="mailto:nowhere@mozilla.org"><img src={require('../../images/icons/gitlab.svg')} alt="hello" /></a></div>
+        <div className="contact__content"><a href="mailto:nowhere@mozilla.org"><img src={require('../../images/icons/gmail.svg')} alt="hello" /></a></div>
       </div>
     </div>
   );
